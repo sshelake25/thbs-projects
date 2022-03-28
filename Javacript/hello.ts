c5578f274f516d408ef07cc70f22b2faaddea15f
Surekha Shelake <shelakesurekha@gmail.com>
2022-03-28 16:23:25 +0530

    added let, const and template based syntaxt

---
+++ Javacript/hello.ts
@@ -0,0 +1,11 @@
+console.log('Hello typescript');
+
+// command to run and transplie to ES6 verrsion
+// C:\Users\DELL\AppData\Roaming\npm\tsc -w -t ES6 ./Javascript/hello.ts
+
+let addtion = (a, b) => { return a + b };
+
+function howareyou() {
+    return ' I am good';
+}
+
