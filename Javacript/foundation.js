f783512c0a961730288b7a0c551f65d99f1d9cdd
Surekha Shelake <shelakesurekha@gmail.com>
2022-03-28 08:56:52 +0530

    variable, functions, concat

---
+++ Javacript/foundation.js
@@ -0,0 +1,24 @@
+/**
+ * Basic around variables inside javascipt
+ */
+let bacthNumber = 99; //number datatype
+console.log(bacthNumber);
+console.log(typeof bacthNumber); //number
+
+bacthNumber = 'nine nine'; // updating value of already declared veriable 
+console.log(bacthNumber); // 
+console.log(typeof bacthNumber); // sting 
+
+
+
+let batchSpeclization = 'MEAN/MERN';
+console.log(batchSpeclization);
+console.log(typeof batchSpeclization);
+
+let allPresent = true; //block scope
+console.log(allPresent); 
+console.log(typeof allPresent);
+
+var isLegacy = 'Yes'; 
+console.log(isLegacy); // veriable with var get access thorught window object 
+console.log(typeof isLegacy); //variable will availabe gloablly in program
---
+++ Javacript/functions_contact.js
@@ -0,0 +1,12 @@
+function addTypes(avar1 , avar2) { //defination of function
+    return avar1 + avar2;
+}
+
+
+console.log( addTypes('Priya', 'Reddy') ); //  'PriyaReddy'
+
+console.log( addTypes('Priya', 20) ); //  'Priya20'
+
+console.log( addTypes(300, 'Movies') ); //  '300Movies'
+
+console.log( addTypes('20', 20) ); //  2020
