d676686a0e3382e39c1809fdd3c696d42167af96
Surekha Shelake <shelakesurekha@gmail.com>
2022-03-28 10:32:55 +0530

    added object basics

--- Javacript/array.js
+++ Javacript/array.js
@@ -1,10 +1,8 @@
 let contactList = []; //declear of empty array 
 console.log(typeof contactList) // object
 console.log(contactList);
-
 let contactL = new Array(); // declear array with new keyword
 console.log(contactL);
-
 contactL = ['ten', 20, true, 'hello']; // initlization or assing value to array
 console.log(contactL);
 
@@ -14,8 +12,6 @@ console.log(classList);
 //accssing value from array;
 console.log(classList[2]); // classList[2]
 console.log(classList["1"]); // index as string qoeutes
-//update values inside arraya 
-//fist access array postion and = to assing new value.
-
+//update values inside arraya //fist access array postion and = to assing new value.
 classList[2] = "THIS";
 console.log(classList);
